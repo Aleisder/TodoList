04cfe71511bd99cf89a7af8af7c942d11a18a68e
Aleisder <tsarenko_d@inbox.ru>
2022-09-10 11:54:16 +0300

    Made animation, not fully localization, fixed bug fragment for adding new todo

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.todolist
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.todolist.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,6 +25,18 @@ class MainActivity : AppCompatActivity() {
             //disable the item-placeholder in menu
             menu.getItem(1).isEnabled = false
         }
+
+
+        val navController = findNavController(R.id.fragment_container)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        binding.fabAddNewTodo.setOnClickListener {
+            navController.navigate(R.id.action_todoListFragment_to_addNewTodoFragment)
+            Toast.makeText(this, "Add new Todo", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 }
